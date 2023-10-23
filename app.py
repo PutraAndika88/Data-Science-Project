7cebe5cf75f724df3125aff5ffece50a358a2ac7
puth88 <133973000+PutraAndika88@users.noreply.github.com>
2023-10-23 23:02:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pickle
 import os
 
 # Membuka file model
-customer_churn_model = pickle.load(open(customer_churn.pkl, 'rb'))
+customer_churn_model = pickle.load(open(customer_churn.sav, 'rb'))
 
 
 st.title("Customer Churn Prediction using Machine Learning")
